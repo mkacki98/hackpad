f153352df9633506f64630fadd9f7c80124e0c59
Miko <mikolajkacki98@gmail.com>
2024-06-04 14:01:23 +0100

    Daybook template update.

--- src/templates/daybookTemplate.tsx
+++ src/templates/daybookTemplate.tsx
@@ -1,1 +1,5 @@
-export const daybookTemplate = '<h2>🧠 Keep in mind <h2> \n\n <h2>🐥 Standup <h2>';
+
+
+export const daybookTemplate = '<h2>🧠 Keep in mind <h2> \n\n ✅ Today\'s tasks \n\n <h2>🐥 Standup <h2>';
+
+
